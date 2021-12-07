756273125b3451207b0080c857a69a3967b33b18
Chandler Davidson <chandlermdavidson@gmail.com>
2021-12-07 09:08:11 -0600

    Add day 6
    Add day 6

---
+++ day/6/part/1/index.ts
@@ -0,0 +1,23 @@
+import { getInput, range, sum } from 'utils';
+
+const initialFish = getInput().split(',').map(f => parseInt(f));
+const fishCount = range(9).map(() => 0);
+
+// Fill fish
+for (const fish of initialFish) {
+  fishCount[fish] += 1;
+}
+
+// Simualte days
+for (let i = 1; i <= 80; i++) {
+  const zeros = fishCount[0];
+
+  for (let i = 0; i < 8; i++) {
+    fishCount[i] = fishCount[i + 1];
+  }
+
+  fishCount[6] += zeros;
+  fishCount[8] = zeros;
+}
+
+console.log(`day: ${i}, fish: ${sum(fishCount)}`);
