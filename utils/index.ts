e23f292b6069f8eba91f67f0a33c999543be09c7
Chandler Davidson <chandlermdavidson@gmail.com>
2021-12-07 16:53:42 -0600

    Add day 7

--- utils/index.ts
+++ utils/index.ts
@@ -36,6 +36,29 @@ export function sum(arr: number[]) {
   return arr.reduce((acc, el) => acc + el, 0);
 }
 
+export function average(arr: number[]): number {
+  const { length } = arr;
+  if (length == 0)
+    throw 'Cannot average array of length 0';
+
+  return sum(arr) / length;
+}
+
+export function median(values: number[]) {
+  if (values.length === 0) throw new Error("No inputs");
+
+  values.sort(function (a, b) {
+    return a - b;
+  });
+
+  const half = Math.floor(values.length / 2);
+
+  if (values.length % 2)
+    return values[half];
+
+  return (values[half - 1] + values[half]) / 2.0;
+}
+
 export function chunk<T>(arr: T[], size: number): T[][] {
   return arr.reduce((acc, el, i) => {
     const chunkIndex = Math.floor(i / size);
@@ -47,4 +70,18 @@ export function chunk<T>(arr: T[], size: number): T[][] {
 
 export function create2DArray<T>(size: number, value: T): T[][] {
   return Array(size + 1).fill(null).map(() => Array(size + 1).fill(value));
+}
+
+export function memo<Y>(func: (input: string) => Y): (input: string) => Y {
+  const cache = {} as { [key: string]: Y };
+
+  return function (input: string): Y {
+    if (input in cache)
+      return cache[input];
+
+    const value = func(input);
+    cache[input] = value;
+
+    return value;
+  }
 }
